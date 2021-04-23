6a1fa99c7038b9c73ace6198a35f1bf3818a2baf
Lucas Rodrigues <lucasrafaelor@hotmail.com>
2021-04-23 17:50:28 -0300

    fix login

--- src/main/java/br/edu/ifpb/pweb2/controller/LoginController.java
+++ src/main/java/br/edu/ifpb/pweb2/controller/LoginController.java
@@ -1,6 +1,7 @@
 package br.edu.ifpb.pweb2.controller;
 
 import java.io.Serializable;
+import java.util.List;
 
 import javax.inject.Inject;
 
@@ -23,6 +24,11 @@ public class LoginController implements Serializable {
 	}
 
 	private PasswordUtil passwordUtil;
+
+	public List<UsuarioAdmin> findAll()
+	{
+		return this.usuarioDAO.findAll();
+	}
 
 	public UsuarioAdmin isValido(String usuario, String senha) {
 
